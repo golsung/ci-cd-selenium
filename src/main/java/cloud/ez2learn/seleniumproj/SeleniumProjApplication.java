da3e3412e30f2af49dafc20421c3728b77e9ea5a
정인상 <golsung@naver.com>
2023-03-08 11:09:36 +0900

    first commit

---
+++ src/main/java/cloud/ez2learn/seleniumproj/SeleniumProjApplication.java
@@ -0,0 +1,13 @@
+package cloud.ez2learn.seleniumproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/ez2learn/seleniumproj/SeleniumProjApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.ez2learn.seleniumproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
