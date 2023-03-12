30406cdd05bf8be86404026b53aa2c1fecae7005
정인상 <golsung@naver.com>
2023-03-12 19:57:15 +0900

    added workflow for unit and api tests

--- src/main/java/cloud/ez2learn/seleniumproj/CalculatorController.java
+++ src/main/java/cloud/ez2learn/seleniumproj/CalculatorController.java
@@ -1,7 +1,5 @@
 package cloud.ez2learn.seleniumproj;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
