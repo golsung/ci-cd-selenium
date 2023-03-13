3c7317baa86cca7440b7f494f096929d5ed40566
정인상 <golsung@naver.com>
2023-03-13 17:02:09 +0900

    deploy to staging env

--- start.sh
+++ start.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-#PROJECT_ROOT="/home/ubuntu/app"
-#JAR_FILE="$PROJECT_ROOT/calc.jar"
-#
-#APP_LOG="$PROJECT_ROOT/application.log"
-#ERROR_LOG="$PROJECT_ROOT/error.log"
-#DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
-
-TIME_NOW=$(date +%c)
-
-# build 파일 복사
-#echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+cp ./build/libs/selenium*.jar ./calc.jar
 
 # jar 파일 실행
-#echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE &
+nohup java -jar ./calc.jar &
 
-#CURRENT_PID=$(pgrep -f $JAR_FILE)
-#echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
