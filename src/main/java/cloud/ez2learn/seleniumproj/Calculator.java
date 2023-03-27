1dc58e99008877899589abefbb9e463d3f2bdd21
정인상 <golsung@naver.com>
2023-03-27 10:28:42 +0900

    undo changes for lab

--- src/main/java/cloud/ez2learn/seleniumproj/Calculator.java
+++ src/main/java/cloud/ez2learn/seleniumproj/Calculator.java
@@ -28,7 +28,7 @@ public class Calculator {
     public void setRightOperand(int b) {
         this.b = b;
     }
-//    public int getDifference() {
+//    public int getDifference() { //
 //         this.result = a - b;
 //         return this.result;
 //    }
