6a5af19ec07bf2aab98aa979e57b0931f4838ee2
정인상 <golsung@naver.com>
2023-03-27 15:53:43 +0900

    added diff methof and tests

--- src/test/java/unitTests/CalculatorTest.java
+++ src/test/java/unitTests/CalculatorTest.java
@@ -35,19 +35,19 @@ class CalculatorTest {
         assertEquals(25, calculator.getSum());
     }
 
-//     @Test
-//     public void testDifference1() {
-//         Calculator calculator = new Calculator();
-//         calculator.setLeftOperand(12);
-//         calculator.setRightOperand(13);
-//         assertEquals(-1, calculator.getDifference());
-//     }
-//
-//    public void testDifference2() {
-//        Calculator calculator = new Calculator();
-//        calculator.setLeftOperand(0);
-//        calculator.setRightOperand(5);
-//        assertEquals(-5, calculator.getDifference());
-//    }
+     @Test
+     public void testDifference1() {
+         Calculator calculator = new Calculator();
+         calculator.setLeftOperand(12);
+         calculator.setRightOperand(13);
+         assertEquals(-1, calculator.getDifference());
+     }
+
+    public void testDifference2() {
+        Calculator calculator = new Calculator();
+        calculator.setLeftOperand(0);
+        calculator.setRightOperand(5);
+        assertEquals(-5, calculator.getDifference());
+    }
 
 }
